44c605ee53a415c74fb91c5ec8c2773f72b642d2
Natchan03 <natchan_ca@hotmail.com>
2021-07-20 00:50:47 -0400

    Renamed notes.html to take-notes.html

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/signup", "/user", "/tutorial", "/featureintro", "/test","/feedback","/layoutTest").permitAll()
+		http.authorizeRequests().antMatchers("/", "/signup", "/user", "/tutorial", "/featureintro", "/take-notes","/feedback","/layoutTest").permitAll()
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
 				.permitAll().and().httpBasic();
 	}
